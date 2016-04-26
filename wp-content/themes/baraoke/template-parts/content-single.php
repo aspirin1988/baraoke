c5cacc237e208246c28c9869e0b1040cc3c29f67
Sergey <aspirin_1988@mail.ru>
2016-04-26 17:58:02 +0600

    /start

--- wp-content/themes/baraoke/template-parts/content-single.php
+++ wp-content/themes/baraoke/template-parts/content-single.php
@@ -17,22 +17,17 @@ $cat=get_the_category( $post->ID );
 	<div class="wrapper">
 		<div class="content">
 			<section class="services-section uk-grid">
-				<?php $post=get_post();
-				$cat=get_the_category( $post->ID );
-				$post=get_posts($args1 = array( 'cat'=> $cat[0]->cat_ID ,'numberposts'=>20, )); ?>
-				<div class="uk-width-1-1 uk-width-medium-1-3 uk-width-large-1-3">
-					<ul class="uk-list">
-						<?php /*print_r($post);*/ foreach($post as $key=>$value ): ?>
-							<li class="uk-text-center"><a <?php if($curent->ID==$value->ID){ echo 'style="text-shadow: #fff 0px 0px 12px"';} ?> href="<?php echo $value->guid;?>" ><?php echo $value->post_title;?></a></li>
-						<?php endforeach; ?>
-					</ul>
-				</div>
-				<div class="services-image uk-width-1-1 uk-text-center uk-width-medium-2-3 uk-width-large-2-3">
-					<img src="<?php echo get_the_post_thumbnail_url($curent->ID,'full')?>" alt="">
+				<div  class="services-image uk-width-1-1  uk-text-center uk-width-medium-1-1 uk-width-large-1-1">
+					<div style=" background-size: cover; background-image: url(<?=get_the_post_thumbnail_url($curent->ID,'full')?>)">
+						<h2 style="padding: 80px 30px; " class="mct uk-text-center wow slideInLeft">
+							<p style="background: rgba(0,0,0,0.7);display: inline-block;padding: 5px;" >
+								<?=$curent->post_title?>
+							</p>
+						</h2>
+					</div>
 				</div>
 
 				<div class="services-content uk-width-1-1">
-					<h2 class="mct uk-text-center wow slideInLeft"><?=$curent->post_title?></h2>
 					<p><?=$curent->post_content?></p>
 				</div>
 			</section>
