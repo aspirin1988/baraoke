ff75cf518278ef68ec5f630ae0dc7980e51b2fc0
Sergey <aspirin_1988@mail.ru>
2016-05-03 12:46:08 +0600

    /start

--- wp-content/themes/baraoke/template-parts/content-gallery.php
+++ wp-content/themes/baraoke/template-parts/content-gallery.php
@@ -1,6 +1,5 @@
 
 <section class="slide-section gallery-page comments">
-	<h1 class="slide-section-title uk-text-center">Галерея</h1>
 	<?php $gl_cat=explode(',',get_field('gallery')); foreach($gl_cat as $val): ?>
 	<h2 class="gallery-page-title2"><?php echo $val; ?></h2>
 	<ul class="uk-grid uk-grid-width-1-2 uk-grid-width-medium-1-2 uk-grid-width-large-1-4" data-uk-grid-margin>
